fc41be2a48e9376596545d7199a3a0c5ebd32292
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-01-05 22:18:08 +0900

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var composer = require('gulp-composer');
+var minimist = require('minimist');
+
+var knownOptions = {
+  string: 'path',
+  default: { path: 'dist' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('build', function () {
     return gulp.src('src/*', {base: "./src"})
@@ -11,7 +19,12 @@ gulp.task('build', function () {
 gulp.task('zip', function () {
     return gulp.src(['build/*','!build/composer.*','build/**/*'], {base: "./build"})
         .pipe(zip('wp-c5-exporter.zip'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(options.path));
+});
+
+gulp.task('mv', function () {
+    return gulp.src(['build/*','!build/composer.*','build/**/*'], {base: "./build"})
+        .pipe(gulp.dest(options.path));
 });
 
 gulp.task('default', ['build', 'zip']);
